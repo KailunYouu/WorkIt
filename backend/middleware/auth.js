9031c8f4d0d880ca65b0e810fd05e8df3fbc337d
Omaima <omaimasharhan@gmail.com>
2026-05-03 13:12:49 -0400

    Updated auth to use Authorization Bearer format

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,16 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.header("token");
+  const authHeader = req.header("Authorization");
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ message: "Auth Error" });
   }
+const token = authHeader.startsWith("Bearer ")? authHeader.slice(7):null;
 
+if(!token){
+  return res.status(401).json({ message: "Invalid Token Format"});
+}
   try {
     const decoded = jwt.verify(token, "randomString");
     req.user = decoded.user;
